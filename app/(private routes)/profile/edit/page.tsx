a5355ecd225defa5b2026585f3e84358d462db6d
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2026-01-18 22:06:07 +0200

    v2

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -1,27 +1,18 @@
 import EditProfile from './EditProfile.client';
 import { getMeServer, checkServerSession } from '@/lib/api/serverApi';
-import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export default async function EditProfilePage() {
-  const cookieStore = await cookies();
+  
+  
 
-  const cookieHeader = [
-    cookieStore.get('accessToken')?.value &&
-      `accessToken=${cookieStore.get('accessToken')?.value}`,
-    cookieStore.get('refreshToken')?.value &&
-      `refreshToken=${cookieStore.get('refreshToken')?.value}`,
-  ]
-    .filter(Boolean)
-    .join('; ');
-
-  const session = await checkServerSession(cookieHeader);
+  const session = await checkServerSession();
 
   if (!session) {
     redirect('/auth/signin');
   }
 
-  const user = await getMeServer(cookieHeader);
+  const user = await getMeServer();
 
   return <EditProfile serverUser={user} />;
 }
