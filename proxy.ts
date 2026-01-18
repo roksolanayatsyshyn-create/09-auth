b16097d6eadfb63aad5533bb7f909ec27d06f540
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2026-01-18 17:51:33 +0200

    proxy function rename

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 const privateRoutes = ['/profile', '/notes'];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isPrivate = privateRoutes.some((route) => pathname.startsWith(route));
